c9059bfea7e8b4f101332d6300b086e09095c2a2
D-Bhatta <dbhatta1232@gmail.com>
2018-05-03 13:54:17 -0400

    committed server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var articles = {
 'article-one' : {
     title:'Article One | Debabrata',
